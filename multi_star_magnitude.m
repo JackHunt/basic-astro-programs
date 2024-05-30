5b4ab191488f4ebc6f2871bfc9367aba5ba638c6
Jack Hunt <jackmileshunt@gmail.com>
2024-05-30 19:06:26 +0100

    Update multi_star_magnitude.m

--- multi_star_magnitude.m
+++ multi_star_magnitude.m
@@ -1,4 +1,11 @@
 % Compute the apparent or absolute magnitude of a multi star system.
+% 
+% ------------------------------------------------------------------------
+% APPEARED IN ASTRONOMICAL COMPUTING, SKY & TELESCOPE, APRIL, 1984
+% ------------------------------------------------------------------------
+% 
+% Translated from an old BASIC dialect by me (JH).
+
 
 mags = [6.4, 3.1, 2.2];
 
